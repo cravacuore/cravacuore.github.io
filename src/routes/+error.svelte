1e0643f179c11690246cde4570c8511344a60cd5
Damián Cravacuore <cravacuore.dev@gmail.com>
2022-10-06 20:17:14 -0300

    Added simple 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<section class="h-screen bg-gradient-to-b from-gray-500 to-gray-800">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-sm text-center">
+            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-100">404</h1>
+            <p class="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-white">Something's missing.</p>
+            <a href="/" class="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Back to Homepage</a>
+        </div>
+    </div>
+</section>
