fd8ecd27437de589e808884ced3a48570d58f2b3
Damián Cravacuore <cravacuore.dev@gmail.com>
2022-10-06 20:17:32 -0300

    Update docs

---
+++ docs/_app/immutable/chunks/1-0175e027.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-1bda70d2.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-c383afe9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-182d18fa.js";export{t as component};
