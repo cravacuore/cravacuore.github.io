5f98cc3e3bc743b7dff27469dc0738680894a44d
Damián Cravacuore <cravacuore.dev@gmail.com>
2022-10-06 19:09:16 -0300

    Update entire page using svelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs'
+    }),
+    paths: {
+      base: dev ? '' : '/cravacuore.github.io',
+    },
   },
   preprocess: [ preprocess({
       postcss: true,
