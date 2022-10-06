1aed849d3b7a24a8e5f1ed3ad80ecb78d68b13e2
Damián Cravacuore <cravacuore.dev@gmail.com>
2022-10-06 17:52:00 -0300

    Added new home under-construction page

--- cravacuore/src/routes/+page.svelte
+++ cravacuore/src/routes/+page.svelte
@@ -1,3 +1,42 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<div class="flex items-center justify-center h-screen bg-gradient-to-b from-indigo-500 via-purple-500 to-gray-600">
+  <div class="container">
+    <div class="bg-gradient-to-b from-zinc-100 to-zinc-300 rounded-lg shadow-lg p-5 md:p-20 mx-2">
+      <div class="text-center">
+        <h2 class="text-4xl tracking-tight leading-10 font-bold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl uppercase">
+          Cravacuore Damián
+        </h2>
+        <h3 class='text-xl md:text-3xl mt-10'>New website under construction 🚧</h3>
+      </div>
+      <div class="flex flex-wrap mt-10 justify-center">
+        <div class="m-3">
+          <a href="http://cravacuore.com.ar/blog/" target="_blank" title="Blog"
+             class="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-blue-600 hover:border-blue-600 hover:bg-blue-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+            <span class="mx-auto">Blog</span>
+          </a>
+        </div>
+        <div class="m-3">
+          <a href="https://vimeo.com/cravacuore" target="_blank" title="Vimeo"
+             class="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-blue-500 hover:border-blue-500 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+            <span class="mx-auto">Vimeo</span>
+          </a>
+        </div>
+        <div class="m-3">
+          <a href="http://cravacuore.com.ar/photos" target="_blank" title="Fotos"
+             class="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+            <span class="mx-auto">Fotos</span>
+          </a>
+        </div>
+        <div class="m-3">
+          <a href="https://github.com/cravacuore" target="_blank" title="Github"
+             class="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-orange-500 hover:border-orange-500 hover:bg-orange-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+            <span class="mx-auto">Github</span>
+          </a>
+        </div>
+      </div>
+      <div class="text-center">
+        <p class="text-md md:text-xl mt-10">Bienvenido a mi espacio personal en la web.</p>
+        <p class="text-md md:text-xl mt-4">Estás invitado a seguir por la sección que te interese. Gracias por pasar.</p>
+      </div>
+    </div>
+  </div>
+</div>
